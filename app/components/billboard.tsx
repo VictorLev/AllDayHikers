19f00bd4af25fd5fa0719f5f9b5925dcf2fd70ee
Victor Levesque <levesque.vic@gmail.com>
2024-01-19 13:41:08 +1100

    commit

---
+++ app/components/billboard.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react";
+
+
+const Billboard = () => {
+
+    return (
+      <div className="h-[25rem] my-8 ">
+      </div>
+     );
+}
+
+export default Billboard;
---
+++ app/components/container.tsx
@@ -0,0 +1,16 @@
+interface ContainerProps {
+  children: React.ReactNode;
+
+}
+
+const Container: React.FC<ContainerProps> = ( {
+  children
+}) => {
+  return (
+      <div className="flex justify-center max-w-7xl flex-col mx-auto">
+          {children}
+      </div>
+   );
+}
+
+export default Container;
